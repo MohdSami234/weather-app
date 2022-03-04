0aaee6488add5da5aa91cc82ff8825233245f7df
Mohd Sami <msami9503@gmail.com>
2022-03-05 04:44:11 +0530

    weather-app

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-
-//selector variable
 var inputval = document.querySelector('#cityinput');
 var btn = document.querySelector('#add');
 var city = document.querySelector('#cityoutput');
-var descrip = document.querySelector('#description');
+var description = document.querySelector('#description');
 var temp = document.querySelector('#temp');
 var wind = document.querySelector('#wind');
-apik = "3045dd712ffe6e702e3245525ac7fa38"
-//kelvin to celcious
+var apik = "3045dd712ffe6e702e3245525ac7fa38"
+
 function convertion(val){
     return (val - 273).toFixed(2)
 }
-//fetch
+
     btn.addEventListener('click', function(){
         fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
         .then(res => res.json())
